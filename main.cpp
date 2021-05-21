452b76ac449fd720fa2db907a274d3073e839db4
Kevin Rojas <rojaskevin.g@gmail.com>
2021-05-21 17:59:17 -0300

    agregado Main codigo Mati

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
-#include <iostream>
+#define VUELTA (20)
+int main() {
+    int entrenamiento [VUELTAS+1]= {0};
+    int vueltaACargar =  1;
+    int tiempoDeVuelta = 0;
 
-using namespace std;
+    system("cls");
 
-int main () {
- 
- return 0;
+    while ( vueltaACargar != 0 ) {
+        leer(" Ingrese la vuelta a cargar (solo se aceptan valores del 1 al 20) \n Ingre 0 para SALIR: ", vueltaACargar);
+
+        tiempoDeVuelta= cargarDatos(vueltaACargar,tiempoDeVuelta);
+
+        entrenamiento [vueltaACargar] = tiempoDeVuelta;
+
+        if (vueltaACargar != 0)
+        {
+            /* code */
+        } else
+        {
+            system("cls");
+
+            for (int i = 1; i <= VUELTAS; i++)
+            {
+                if (entrenamiento[i] != 0) {
+                   cout <<"Vuelta "<<i<< " : " << entrenamiento[i] << endl;
+                }
+            }
+        }
+    }
+    system ("pause"); 
+    return 0;
 }
